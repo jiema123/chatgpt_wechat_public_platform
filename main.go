bee5ecb4321920578ba116c64475442401d89cc3
fangjiefeng <fangjiefeng@360.cn>
2023-03-10 19:04:11 +0800

    add

--- main.go
+++ main.go
@@ -78,7 +78,10 @@ func wechatNotify(context *gin.Context) {
 			}
 			go handleMsg(msg.FromUserName, msg.Content)
 
-			return messages.NewText("请稍等片刻，AI正在为您查询答案。输入任意字符或按回车键继续查看。")
+			return messages.NewText("请稍等片刻，AI正在为您查询答案。输入任意字符继续查看。" +
+				"\n 使用技巧 普通提问：你想问的任何问题 " +
+				"\n绘图功能：以画图画三个字打头 加上图的描述信息" +
+				"\n 举例子：1. 你是机器人吗  2. 画图画 在写代码的小猫")
 		}
 		//return messages.NewText("not supper")
 		return kernel.SUCCESS_EMPTY_RESPONSE
